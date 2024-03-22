0eac896672433a17475d90cf4b6aa94038d75d55
zincalex <alessandro.viesp@gmail.com>
2024-03-22 13:51:15 +0100

    ExactOutliers method base done

--- G023HW1.py
+++ G023HW1.py
@@ -1,13 +1,46 @@
 from pyspark import SparkContext, SparkConf
 import sys
 import os
+import time
+import math
 
 def str_to_point(point):
     # Function that takes a string representing a point and returns it as a couple of integers
     return float(point.split(',')[0]), float(point.split(',')[1])
 
 def ExactOutliers(listOfPoints, D, M, K):
+    start_time = time.time()
+
+    outliers = []
+    num_outliers = 0
+    for p in listOfPoints : 
+        for q in listOfPoints : 
+            B_S_p = 0
+            if p != q :
+                dist = math.sqrt((p[0] - q[0])**2 + ((p[1] - q[1])**2))
+                if dist <= D : 
+                    B_S_p += 1
+                
+        
+        if B_S_p <= M : 
+            num_outliers += 1
+            outliers.append((p, B_S_p))
+
+    
+    outliers = sorted(outliers, key=lambda x : x[1])
+    print(outliers)
+
+    print("----------------------------------------------")
+    print("Numbers of (%.2f, %d)-outliers : %d" %(D, M, num_outliers))
+    for k in range(0, K) :   
+        print(outliers[k][0])
+    print("----------------------------------------------")
+    
+
+
+    print("ExactOutliers() running time: --- %s seconds ---" % (time.time() - start_time))
     return
+
 
 def MRApproxOutliers(inputPoints, D, M, K):
     return
@@ -22,15 +55,8 @@ def main():
     sc = SparkContext(conf=conf)
     
     # INPUT READING
-    # 1. Read input file 
-    data_path = sys.argv[1]
-    assert os.path.isfile(data_path), "File not found"
-    rawData = sc.textFile(data_path)                                    # Read input points into an RDD of strings
-    inputPoints = rawData.map(str_to_point).repartition(1).cache()      # Transform the RDD of strings into an RDD of points using the function str_to_point
-    
-    # 2. Read parameters
+    # 1. Read parameters
     D = sys.argv[2]
-    assert isinstance(D, float), "D must be a float"
     D = float(D)
     
     M = sys.argv[3]
@@ -44,6 +70,13 @@ def main():
     L = sys.argv[5]
     assert L.isdigit(), "L must be an integer"
     L = int(L)
+
+    # 2. Read input file 
+    data_path = sys.argv[1]
+    assert os.path.isfile(data_path), "File not found"
+    rawData = sc.textFile(data_path)                                    # Read input points into an RDD of strings
+    inputPoints = rawData.map(str_to_point).repartition(L).cache()      # Transform the RDD of strings into an RDD of points using the function str_to_point
+    numPoints = inputPoints.count()                                     # Total number of points in the RDD
     
     # PRINTING PARAMETERS
     print("Dataset = ", data_path.split("\\")[-1])
@@ -51,11 +84,19 @@ def main():
     print("M = ", M)
     print("K = ", K)
     print("L = ", L)
+    print("Total number of points :", numPoints)
     
-    # EXACT ALGORITHM
+    listOfPoints = inputPoints.collect()
+    if numPoints <= 200000 : # EXACT ALGORITHM
+        ExactOutliers(listOfPoints, D, M, K)
+        
+    # APPROXIMATE ALGORITHM
+    MRApproxOutliers(listOfPoints, D, M, K)
+
+    
     
 
-    # APPROXIMATE ALGORITHM
+    
 
 
 
